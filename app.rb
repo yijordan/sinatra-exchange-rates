3bffde2a4cabc5053fc1df6abc24315dea0a151f
yijordan <yij@uchicago.edu>
2025-02-22 05:22:46 +0000

    got started, about to do http get

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+require "dotenv/load"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  list_url = "https://api.exchangerate.host/list?access_key=" + ENV.fetch(EXCHANGE_KEY)
+  HTTP.get(list_url)
+  @currency = "asdf"
+  erb(:homepage)
 end
