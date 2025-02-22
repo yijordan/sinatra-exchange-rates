bcdf209cdcd3b42926293a88756f52e8f185e639
yijordan <yij@uchicago.edu>
2025-02-22 20:52:52 +0000

    homepage works omg yay!

--- app.rb
+++ app.rb
@@ -5,8 +5,10 @@ require "json"
 require "dotenv/load"
 
 get("/") do
-  list_url = "https://api.exchangerate.host/list?access_key=" + ENV.fetch(EXCHANGE_KEY)
-  HTTP.get(list_url)
-  @currency = "asdf"
+  @list_url = "https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_KEY")
+  @resp = HTTP.get(@list_url)
+  @raw_response = @resp.to_s
+  @parsed_response = JSON.parse(@raw_response)
+  @currencies = @parsed_response.fetch("currencies")
   erb(:homepage)
 end
